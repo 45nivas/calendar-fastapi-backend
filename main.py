f95a9876282cb41e710764883a4497b0c130dbec
45nivas <mattanivas@gmail.com>
2025-07-03 20:13:44 +0530

    first commit

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
-
-# Allow requests from any origin (e.g., your Streamlit frontend)
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],  # Replace with your frontend URL in production
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 @app.get("/")
 def read_root():
